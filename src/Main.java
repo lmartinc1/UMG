80b0dc43023be06b24f74904cb88751533a63d91
lmartinc1 <lmartinc1@miumg.edu.gt>
2025-08-02 10:08:27 -0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        // Imprimir un saludo
+        System.out.println("Hola Mundo desde Java!");
+
+        // Variables
+        int numero1 = 5;
+        int numero2 = 10;
+
+        // Sumar
+        int suma = numero1 + numero2;
+
+        // Mostrar el resultado
+        System.out.println("La suma de " + numero1 + " y " + numero2 + " es: " + suma);
+    }
+}
